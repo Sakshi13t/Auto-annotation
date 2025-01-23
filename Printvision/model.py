7c2623d3c6cb05808647d64018503d6c24d43973
Sakshi Tandon <120265901+Sakshi13t@users.noreply.github.com>
2025-01-23 16:56:41 +0530

    commit

---
+++ Printvision/model.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+
+# Load YOLOv8n model 
+model = YOLO('yolov8n.pt')
+
+# Train the model
+model.train(
+    data=r'C:\Users\Amazin\ultralytics\printvis\dataset.yaml', 
+    epochs=50, 
+    augment=True,                  
+    imgsz=640,                   
+    batch=16,                    # Batch size
+    project='runs/train',        # Save results to runs/train
+    name='custom_yolov8n',       # Model name
+    save_period = 1,
+    device = 'cpu'
+)
+
+# Validate the model on the validation set
+metrics = model.val(data='dataset.yaml')
+print(metrics)
+
+#exporting the model
+model.export(format='onnx') 
+#C:\Users\Amazin\ultralytics\yoloenv\runs\train\custom_yolov8n3\weights\best.pt
+
